5677f76bf10a567992bd91df963aeb0129d3884e
OUTCAST-PC\semen <dismal.forecast@gmail.com>
2025-03-15 22:05:54 +0200

    Adding prettier config file

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+const config = {
+    semi: true,
+    singleQuote: false,
+    trailingComma: 'es5',
+    tabWidth: 2,
+    useTabs: false,
+    bracketSpacing: true,
+    arrowParens: 'always',
+    printWidth: 80, 
+    endOfLine: 'lf',
+  };
+  
+  export default config;
